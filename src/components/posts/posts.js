83153c9aa57ee8356d8461e2ec56ba0da47bf7ab
Dhiraj Patil <mr.dhirajpatil1995@gmail.com>
2022-09-03 13:52:41 +0530

    Loader added within the application

--- src/components/posts/posts.js
+++ src/components/posts/posts.js
@@ -1,11 +1,12 @@
 import React from "react";
 import InfoCard from './post/InfoCard';
+import Loader from '../Loader/Loader';
 import { useSelector } from 'react-redux';
 
 const Posts = ({setCurrentId}) => {
     const posts = useSelector((state)=> state.posts);
     return (
-      !posts.length ? <>Loading</> :<>
+      !posts.length ? <Loader/> :<>
         {posts.map((post)=> {
             return <InfoCard  key={post._id} post={post} setCurrentId={setCurrentId}/>
         })}
